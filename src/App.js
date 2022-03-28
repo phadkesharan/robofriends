bc0d73322c515544ec8ba0bbdf3d2747b1b2c345
Sharan Phadke <phadkesharan@gmail.com>
2022-03-28 22:23:08 +0530

    add Card components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import React from 'react';
-import './App.css';
+import React, { Fragment } from 'react';
+import Card from './Card';
+import data from './robots';
 
-
-class App extends React.Component {
-
-  render() {
+export default function App() {
     return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
+        <Fragment>
+            <h1>Robots</h1>
+            <Card id={data[0].id} name={data[0].name} username={data[0].username} email={data[0].username} />
+            <Card id={data[1].id} name={data[1].name} username={data[1].username} email={data[1].username} />
+            <Card id={data[2].id} name={data[2].name} username={data[2].username} email={data[2].username} />
+        </Fragment>
     )
-
-  }
 }
-
-
-
-export default App;
---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "tachyons";
+
+export default function Card({id, name, username, email}) {
+  return (
+    <div className='bg-light-green dib br3 pa3 ma2 grow'>
+        <img src={`https://robohash.org/${id}`} alt="robots"/>
+        <div>
+            <h2>{id}  {name}</h2>
+            <h2>{username}</h2>
+            <p>{email}</p>
+        </div>
+    </div>
+  )
+}
